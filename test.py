0e50f9b5bb82318604d7ade83cbe76a9e595d388
an1has <129047341+an1has@users.noreply.github.com>
2023-03-27 11:40:13 +0300

    Test
    Test

---
+++ test.py
@@ -0,0 +1,8 @@
+print("Вирахуємо скількі днів ви живете")
+name = input("Ім'я ")
+print("Ведіть свій вік")
+age = int(input("вік: "))
+days = age * 365
+minutes = age * 525948     
+seconds = age * 31556926
+print(name, "проживл(а)", days, "днів", minutes, "хвилин  і", seconds, "секунд!")
